9cec87d3bbae76aae1bccff9bf3d6abb54ae02f3
Richmond <mrgyekye740@gmail.com>
2020-05-07 22:00:00 +0000

    update todo

--- snapyticket/organizer/models.py
+++ snapyticket/organizer/models.py
@@ -5,21 +5,23 @@ from django.urls import reverse
 
 User = settings.AUTH_USER_MODEL
 
+
 class Organizer(models.Model):
     # Todo write a functuality to send the organizer ID to organizers when they regsiter as organizers
     # todo #3 write necesaary querysets for event organizers
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     logo = models.ImageField(upload_to='organizers_logos', default='default_organizer_logo')
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     instagram = models.URLField(blank=True)
-    facebook  = models.URLField(blank=True)
-    telegram  = models.URLField(blank=True)
-    twitter   = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
+    telegram = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
     is_verified = models.BooleanField(default=False)
-    secret_id   = models.SlugField(blank=True)
+    secret_id = models.SlugField(blank=True)
+
     def get_absolute_url(self):
-        return reverse('organizer:update', kwargs={'pk':self.id})
-    
+        return reverse('organizer:update', kwargs={'pk': self.id})
+
     def __str__(self):
         return f'{self.name} - {self.user.username} - {self.secret_id}'
